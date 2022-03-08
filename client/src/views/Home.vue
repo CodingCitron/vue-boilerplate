3f51959ef0588ca5dc50a030ff9135312d7a1a9c
sangmin <xectler@gmail.com>
2022-03-08 18:04:04 +0900

    update

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
     <div>
-        <Banner :image="require('@/assets/image/banner_img_01.jpg')" />
+        <Banner
+            :image="require('@/assets/image/banner_img_01.jpg')"
+            :style="custom"
+        />
     </div>
 </template>
 
 <script>
 import Banner from '@/components/common/Banner'
+import { ref } from 'vue'
 
 export default {
     components: {
         Banner,
     },
 
-    setup() {},
+    setup() {
+        const custom = ref('height: 600px; width: 100%;')
+
+        return {
+            custom,
+        }
+    },
 }
 </script>
 
